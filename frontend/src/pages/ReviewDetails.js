a2215470edd646819018fab699fcb21d2fd108d1
Selami <selamidemir@gmail.com>
2022-10-04 23:29:37 +0300

    güncelleme

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import useFetch from '../hooks/useFetch'
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 function ReviewDetails() {
-  const {reviewId} = useParams()
-  const {loading, error, data} = useFetch(`http://localhost:1337/api/reviews/${reviewId}`)
+  const { reviewId } = useParams();
+  const { loading, error, data } = useFetch(
+    `http://localhost:1337/api/reviews/${reviewId}`
+  );
 
-  if(loading) return <p>Loading...</p>
-  if(error) return <p>{error}</p>
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
-    <div className='review-card'>
-      <div className='rating'>{data.attributes.rating}</div>
+    <div className="review-card">
+      <div className="rating">{data.attributes.rating}</div>
 
       <small>console list</small>
 
       <p>{data.attributes.body}</p>
-      
     </div>
-  )
+  );
 }
 
-export default ReviewDetails
+export default ReviewDetails;
