1acbea4337330ba6699ee43fff2f3ab4de183737
Selami <selamidemir@gmail.com>
2022-10-06 23:39:52 +0300

    kategori bölümü hazırlandı

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {useQuery, gql} from '@apollo/client'
+
+const CATEGORIES = gql`
+  query GetCategories {
+    categories {
+      data {
+        id 
+        attributes {
+          name
+        }
+      }
+    }
+  }
+`;
 
 function SiteHeader() {
+  const { loading, error, data } = useQuery(CATEGORIES)
+
+  if (loading) return <p>loading categories...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
     <div className='site-header'>
         <Link to='/'><h1>Selo Reviews</h1></Link>
+        <nav className='categories'>Filter By Category : 
+          {
+            data.categories.data.map(category => <Link to={`/category/${category.id}`} key={category.id}> {category.attributes.name}</Link>)
+          }
+        </nav>
     </div>
   )
 }
--- frontend/src/pages/Category.js
+++ frontend/src/pages/Category.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { useParams, Link } from "react-router-dom";
+
+const CATEGORY = gql`
+  query GetCategory($id: ID!) {
+    category(id: $id) {
+      data {
+        id
+        attributes {
+          name
+          reviews {
+            data {
+              id
+              attributes {
+                title
+                body
+                rating
+                categories {
+                  data {
+                    id
+                    attributes {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 function Category() {
-  return <div>Category</div>;
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(CATEGORY, {
+    variables: { id: id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      {data.category.data.attributes.reviews.data.map((review, index) => (
+        <div className="review-card" key={index}>
+          <div className="rating">{review.attributes.rating}</div>
+          <h1>{review.title}</h1>
+          {/* {console.log("bir : ", review.attributes.categories.data)} */}
+          {
+            review.attributes.categories.data.map(cat => <small key={cat.id}> {cat.attributes.name} </small> )
+          }
+
+          <p>{review.attributes.body.substring(0, 200)}...</p>
+          <Link to={`/details/${review.id}`}>Read More</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Category;
